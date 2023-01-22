0eaca7c3bd7eacde541b2445baf794f97d18cd4d
trinityzhub <trinity0091+github@gmail.com>
2023-01-22 22:55:09 +0000

    sss

--- test.sh
+++ test.sh
@@ -19,3 +19,20 @@ read -p 'Would you like to add a shortcut to Gnome Launcher? (Y/n): ' GL
 
 git reset --hard && git clean -df &&  git pull
 
+
+
+export user=ubbe group=docker uid=1000 gid=998 && \
+    mkdir -p /home/${user} && \
+ ##   groupadd -g 998 ${group} && \
+ ##   useradd --uid ${uid} --gid ${gid} -d /home/${user} -ms /bin/bash ${user} && \
+    chown -R ${user}:${group} /home/${user}
+
+
+
+export user="codespace" group="dev" uid=1000 gid=998 && \
+    mkdir -p /home/${user} && \
+    groupadd -g 998 ${group} && \
+    useradd --uid ${uid} --gid ${gid} -d /home/${user} -ms /bin/bash ${user} && \
+    chown -R ${user}:${group} /home/${user}
+
+    
