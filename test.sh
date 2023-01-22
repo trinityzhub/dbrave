66c46b64d7b0f669612c3a56e51d1154519681a6
trinityzhub <trinity0091+github@gmail.com>
2023-01-22 23:21:31 +0000

    test123

--- test.sh
+++ test.sh
@@ -20,6 +20,8 @@ read -p 'Would you like to add a shortcut to Gnome Launcher? (Y/n): ' GL
 git reset --hard && git clean -df &&  git pull
 
 
+less /etc/passwd
+
 
 export user=ubbe group=docker uid=1000 gid=998 && \
     mkdir -p /home/${user} && \
@@ -35,4 +37,21 @@ export user="codespace" group="dev" uid=1000 gid=998 && \
     useradd --uid ${uid} --gid ${gid} -d /home/${user} -ms /bin/bash ${user} && \
     chown -R ${user}:${group} /home/${user}
 
+
+
+
+sudo chown $USER:docker $HOME/.local/bin/dbrave-spring
+sudo chown $USER:docker $HOME/.local/share/applications/dbrave-spring.desktop
+
+
+sudo chmod +x   $HOME/.local/bin/dbrave-spring
+sudo chmod +x   $HOME/.local/share/applications/dbrave-spring.desktop
     
+
+
+cd $HOME/.local/bin
+./dbrave-spring
+
+
+
+
